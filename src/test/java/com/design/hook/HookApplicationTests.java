772041509fab7e9bbd7bc65bebb21027c625769a
Laura <laura_habdul@hotmail.fr>
2024-05-09 16:12:52 +0200

    Initial commit

---
+++ src/test/java/com/design/hook/HookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.hook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
