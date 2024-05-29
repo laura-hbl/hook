a3fb581c7102a1192d9d4e9dd663fde76c74f342
Laura <laura_habdul@hotmail.fr>
2024-05-29 12:08:52 +0200

    update Pattern and PatternDTO

---
+++ src/main/java/com/design/hook/dto/PatternDTO.java
@@ -0,0 +1,73 @@
+package com.design.hook.dto;
+
+public class PatternDTO {
+
+    private long id;
+    private String title;
+    private String level;
+
+    private String imageUrl;
+
+    private String createdDate;
+    private long likes;
+    private String description;
+
+    public PatternDTO() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public long getLikes() {
+        return likes;
+    }
+
+    public void setLikes(long likes) {
+        this.likes = likes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
--- src/main/java/com/design/hook/model/Pattern.java
+++ src/main/java/com/design/hook/model/Pattern.java
@@ -1,14 +1,26 @@
 package com.design.hook.model;
 
-import java.sql.Timestamp;
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "patterns")
 public class Pattern {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
     private String title;
     private String level;
-    private Timestamp createdDate;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "created_date")
+    private String createdDate;
     private long likes;
     private String description;
 
@@ -27,7 +39,7 @@ public class Pattern {
         return level;
     }
 
-    public Timestamp getCreatedDate() {
+    public String getCreatedDate() {
         return createdDate;
     }
 
@@ -39,18 +51,48 @@ public class Pattern {
         return description;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setLikes(long likes) {
+        this.likes = likes;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Pattern pattern = (Pattern) o;
-        return id == pattern.id && likes == pattern.likes && Objects.equals(title, pattern.title)
-                && Objects.equals(level, pattern.level) && Objects.equals(createdDate, pattern.createdDate)
-                && Objects.equals(description, pattern.description);
+        return id == pattern.id && likes == pattern.likes && Objects.equals(title, pattern.title) && Objects.equals(level, pattern.level) && Objects.equals(imageUrl, pattern.imageUrl) && Objects.equals(createdDate, pattern.createdDate) && Objects.equals(description, pattern.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, level, createdDate, likes, description);
+        return Objects.hash(id, title, level, imageUrl, createdDate, likes, description);
     }
 }
